0bd678576b53659082893c7ae5f297fa2f0f1926
Roy Krishnan <60521440+roykrishnan@users.noreply.github.com>
2024-08-21 23:38:03 -0400

    Update 2_Hitting.py
    broken button remove

--- pages/2_Hitting.py
+++ pages/2_Hitting.py
@@ -33,14 +33,5 @@ def main():
     # Use st.markdown with unsafe_allow_html for the JavaScript and countdown
     st.markdown(redirect_js, unsafe_allow_html=True)
     
-    # Add a button for manual redirection
-    if st.button("Redirect Now"):
-        js = f"""
-        <script>
-        window.location.href = "{hitting_app_url}";
-        </script>
-        """
-        st.components.v1.html(js)
-
 if __name__ == "__main__":
     main()
