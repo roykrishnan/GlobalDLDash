0aaddcff473907e63f3989bd8bef0ec14e5770d4
Roy Krishnan <60521440+roykrishnan@users.noreply.github.com>
2024-08-21 23:38:25 -0400

    Update 1_Pitching.py
    broken button remove

--- pages/1_Pitching.py
+++ pages/1_Pitching.py
@@ -32,15 +32,6 @@ def main():
     
     # Use st.markdown with unsafe_allow_html for the JavaScript and countdown
     st.markdown(redirect_js, unsafe_allow_html=True)
-    
-    # Add a button for manual redirection
-    if st.button("Redirect Now"):
-        js = f"""
-        <script>
-        window.location.href = "{pitching_app_url}";
-        </script>
-        """
-        st.components.v1.html(js)
 
 if __name__ == "__main__":
     main()
